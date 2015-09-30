bdfa8f4efb235e7711e46176dfb8663a58bd660b
mcflurryxd96 <daniel.mcmurray@myemail.indwes.edu>
2015-09-30 19:21:45 +0000

    changes

--- piggetty.py
+++ piggetty.py
@@ -5,30 +5,61 @@
 
 # Define a function called piggy(string) that returns a string
 
+vowels = "aeiouAEIOU"
+
 def piggy(word):
+	n = 0
+	endword = ""
 	
-	# Magic Happens Here
-	pig = word
-	# Ignore previous line
-	
-	return pig
+	for letter in word:
+		# Check if letter is a vowel
+		if letter in vowels:
+			if n == 0:			# True?  We are done
+				pig = word + "yay"
+				break
+		#		return pig
+			else:
+				pig = word[n:] = endword = "ay"
+				break
+			#	return pig
+		
+		else:
+			# False? Consonant
+			endword += word[n]
+			n = n + 1
+	print(pig)
 
 # Open the file *getty.txt* for reading.  
-gettyfile = open("getty.txt", "r")
+infile = open("getty.txt", "r")
+
 # Open a new file *piggy.txt* for writing.  
-piggyfile = open("piggy.txt", "w")
+outfile = open("piggy.txt", "w")
+
 # Read the getty.txt file into a string.  
+gettystring = infile.read()
 
 # Strip out bad characters (, - .).  
+gettystring = gettystring.replace (",","")
+gettystring = gettystring.replace ("-","")
+gettystring = gettystring.replace (".","")
 
 # Split the string into a list of words.  
+gettylist = gettystring.split ()
 
 # Create a new empty string.  
+piggystring = ""
 
 # Loop through the list of words, pigifying each one.  
+for word in gettylist:
 
 # Add the pigified word (and a space) to the new string.  
+	if len(word) > 0
+		piggystring = piggystring + piggy(word) + " "
+
 
 # Write the new string to piggy.txt.  
+print(piggystring, file = outfile)
 
 # close the files.
+infile.close()
+outfile.close()
